3e7724b0b4fde006f6a7e6b1c202fdc790ad9b15
ekko-wang <15611637909@163.git config --global user.email 15611637909@163.comgit config --global user.email 15611637909@163>
2019-06-04 13:07:06 +0800

    begin

---
+++ components/public/header/geo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="m-geo">
+        <i class="el-icon"/>北京市
+        <nuxt-link
+        class="changeCity"
+        to="/changeCity"
+        >切换城市</nuxt-link>
+        [想和 天津]
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        mt-app
+        ekko-app
       </h1>
       <h2 class="subtitle">
         My incredible Nuxt.js project
